89ce699c5118614f85a03dca10a6c07c6aac0d69
JW <james@luppoleto.com>
2017-09-01 16:11:50 -0400

    Fixed responsive image issues. Removed validation code entirely.

--- scripts/freelancer.js
+++ scripts/freelancer.js
@@ -28,25 +28,6 @@
         offset: {
             top: 100
         }
-    })
-
-    // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
-    });
-
-    // JW: Wire-up Woodburn & Portrait inquiry buttons
-    $('#woodburnButton').click(function(event) {
-        $("#woodburnCheckbox").prop("checked", true);        
-    }); 
-    $('#paintingButton').click(function(event) {
-        $("#paintingCheckbox").prop("checked", true);        
-    }); 
+    })    
 
 })(jQuery); // End of use strict
