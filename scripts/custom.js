215e4480c6cf2d0908b0fd07f2bc6519fba68463
JW <james@luppoleto.com>
2017-09-13 23:00:13 -0400

    Commented hash adding code. Added method to remove hash but disabled for now.

--- scripts/custom.js
+++ scripts/custom.js
@@ -12,9 +12,17 @@
     }); 
 
     // JW: Add hash to the url so that the back button works to get rid of modals
+    // Add #testimonial
     $('.testimonial-modal').on('show.bs.modal', function(e) {
         window.location.hash = "testimonial";            
     });
+    // Remove testimonial hash when closing with a button
+    // $('.testimonial-modal').on('hidden.bs.modal', function(e) {
+    //     if(window.location.hash == "#testimonial") {
+    //         window.location.hash = "clients";            
+    //     }
+    // });
+    // If changing from testimonial to something else -> hide testimonial
     $(window).on('hashchange', function (event) {
         if(window.location.hash != "#testimonial") {
             $('.testimonial-modal').modal('hide');
